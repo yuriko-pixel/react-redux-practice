2ca1925f8958cd8abe4baf86e4e154dbf97b3dd8
yuriko-pixel <beljar731@gmail.com>
2021-07-15 14:03:19 +0900

    updated

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,27 @@
+let count = 0;
+
 const rootReducer = (state = [], action) => {
     switch (action.type) {
       case "ADD_TODO":
-        return [...state, { todo: action.text }];
+        return [...state, { id: count++, todo: action.text, completed: false, deleted: false }];
+      case "COMPLETE_TODO":
+        let todo = [];
+        state.forEach(item => {
+          if(item.id === action.id)
+            item.completed = true;
+          
+          todo.push(item);
+        })
+          return todo;
+      case "DELETE_TODO":
+        let todoarr = [];;
+        state.forEach(item => {
+          if(item.id === action.id)
+            item.deleted = true;
+          
+            todoarr.push(item);
+        })
+          return todoarr;
       default:
         return state;
     }
